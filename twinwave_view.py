ac8f673ab32797c2fd2f92ca810c3b41ab31d838
Deep Gopani <dgopani@splunk.com>
2022-03-10 13:15:36 +0530

    Dark theme logo, licenses, and other best practices changes

--- twinwave_view.py
+++ twinwave_view.py
@@ -1,3 +1,18 @@
+# File: phtwinwave.py
+#
+# Copyright (c) TwinWave, 2022
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions
+# and limitations under the License.
+
 def _tree_order_resources(current_node, ordered_resources=None, depth=0):
     if not ordered_resources:
         ordered_resources = []
